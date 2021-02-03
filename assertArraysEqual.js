942f7ff89016ef428be780cd2a11d8bc190e078a
Evan <evan_quirk@me.com>
2021-02-03 04:16:09 +0000

    lint file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,9 +3,9 @@ const eqArrays = function(array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
   }
   return true;
 };
@@ -18,4 +18,4 @@ const assertArraysEqual = function(array1, array2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3])
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
