24392aa6074d149c94849d9320d687d70a7de073
Evan <evan_quirk@me.com>
2021-02-08 23:17:11 +0000

    add mocha and chai to middle and tail tests

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,4 +1,8 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 4, 6, 9, 10]), [ 4, 6])
+describe('#middle', () => {
+  it("returns '4, 6' from an array of [1, 2, 4, 6, 9, 10]", () => {
+    assert.deepEqual(middle([1, 2, 4, 6, 9, 10]), [ 4, 6]);
+  })
+});
--- test/tail.test.js
+++ test/tail.test.js
@@ -1,6 +1,8 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail  = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+describe('#tail', () => {
+  it("returns 'lighthouse', 'labs' for ['hello', 'lighthouse', 'labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  })
+});
